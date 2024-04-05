773622c1ba9ff740d04a6e202c3e4b98f9156535
Kerry Kapija <oalantonioayllon@gmail.com>
2024-04-05 08:00:37 +0200

    hghgg

---
+++ js/imc.js
@@ -0,0 +1,15 @@
+let btnCalcular = document.getElementById("btnCalcular");
+
+btnCalcular.addEventListener("click", clickBtnCalcular);
+
+function clickBtnCalcular(){
+    let textopeso = document.getElementById("textopeso");
+    let peso = textopeso.value;
+
+    let textoaltura = document.getElementById("textoaltura");
+    let altura = textoaltura.value;
+
+    let imc = peso / (altura * altura);
+
+    alert("El IMC es: " + Math.round(imc));
+}
